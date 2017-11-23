38a3f53092a40dbe863a21e6db909a213153d388
Bartlomiej Nowakowski <bartlomiej.nowakowski@eo.pl>
2017-11-23 19:10:53 +0100

    add arduino and unity code

---
+++ unity/wolverine/Assets/HandMovement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+public class HandMovement : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("COM3", 9600);
+	private double amountToMove;
+	public float gX, gY, gZ, aX, aY, aZ;
+
+	Vector3 actualImuInput = Vector3.zero;
+
+	void Start () {
+		stream.Open ();
+		stream.ReadTimeout = 2000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!stream.IsOpen) 
+			stream.Open ();
+	
+
+		StartCoroutine
+			(
+				AsynchronousReadFromArduino
+				(   (string s) => Debug.Log(s),     // Callback
+					() => Debug.LogError("Error!"), // Error callback
+					10f                             // Timeout (seconds)
+				)
+			);
+
+		if (Input.GetKeyDown(KeyCode.K)) {
+			this.gX = 0.0f;
+			this.gY = 0.0f;
+			this.gZ = 0.0f;
+		}
+//			transform.rotation = Quaternion.Euler (0, 0, 0);
+
+
+//		transform.localEulerAngles = new Vector3 (-gX, -gY, -gZ);
+
+		transform.localPosition = new Vector3 (-aX, -aY, -aZ);
+	}
+
+	public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity){
+		DateTime initialTime = DateTime.Now;
+		DateTime nowTime;
+		TimeSpan diff = default(TimeSpan);
+
+		char delimeterChar = ',';
+		string dataString = null;
+		string[] imuValues;
+
+		do {
+			try {
+				dataString = stream.ReadLine();
+			}
+			catch (TimeoutException) {
+				dataString = null;
+			}
+
+			if (dataString != null)
+			{
+				imuValues = dataString.Split(delimeterChar);
+				this.gX += float.Parse(imuValues[0]) / 3000;
+				this.gY += float.Parse(imuValues[2]) / 3000;
+				this.gZ += float.Parse(imuValues[1]) / 3000;
+
+				this.aX += float.Parse(imuValues[3]) / 190;
+				this.aY += float.Parse(imuValues[5]) / 190;
+				this.aZ += float.Parse(imuValues[4]) / 190;
+
+				callback(dataString);
+
+				yield return null;
+			} else
+				yield return new WaitForSeconds(0.05f);
+
+			nowTime = DateTime.Now;
+			diff = nowTime - initialTime;
+
+		} while (diff.Milliseconds < timeout);
+
+		if (fail != null)
+			fail();
+		yield return null;
+
+	}
+
+	private void calibrate() {
+
+
+	}
+
+}
---
+++ unity/wolverine/Assets/MeleeSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeSystem : MonoBehaviour {
+
+	public int Damage = 50;
+	public double Distance;
+    public double MaxDistance = 2.5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)) {
+                Distance = hit.distance;
+                if(Distance < MaxDistance) {
+                    hit.transform.SendMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+                }
+
+            }
+
+        }
+	}
+}
